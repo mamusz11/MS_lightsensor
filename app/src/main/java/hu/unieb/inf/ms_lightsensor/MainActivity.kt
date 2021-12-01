147ed9b4c6549ff2255a4f630bc66256f7f13d6a
Szilagyi <sz.joci12@gmail.com>
2021-12-01 21:15:57 +0100

    feat: setting up the lightsensor

--- app/src/main/java/hu/unieb/inf/ms_lightsensor/MainActivity.kt
+++ app/src/main/java/hu/unieb/inf/ms_lightsensor/MainActivity.kt
@@ -1,11 +1,41 @@
 package hu.unieb.inf.ms_lightsensor
 
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatDelegate
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    private lateinit var sensorManager: SensorManager
+    private var fenyero: Sensor? = null
+    private lateinit var text: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        text = findViewById(R.id.luxvalue)
+
+        setUpSensor()
+    }
+    private fun setUpSensor() {
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+
+        fenyero = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
+    }
+
+    override fun onSensorChanged(p0: SensorEvent?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+        TODO("Not yet implemented")
     }
 }
